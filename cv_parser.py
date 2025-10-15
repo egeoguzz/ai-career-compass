70255752d741c3d11bc328d032e7e2dd4fd7ce79
Ege Oğuz <iegeoguz@gmail.com>
2025-10-15 17:20:38 +0200

    implement parse pdf function

---
+++ cv_parser.py
@@ -0,0 +1,10 @@
+import fitz
+import docx
+
+def extract_text_from_pdf(file_bytes: bytes) -> str:
+    text = ""
+    with fitz.open(stream=file_bytes, filetype="pdf") as doc:
+        for page in doc:
+            text += page.get_text()
+    return text
+
